88779bca26d211d0f9898d48e22c409b9df54093
Nicolas Delgado <32339920+nadelgadoc@users.noreply.github.com>
2017-10-11 10:34:28 -0300

    Add files via upload

---
+++ main32.py
@@ -0,0 +1,1 @@
+# This file is executed on every boot (including wake-boot from deepsleep)import gcimport machineimport osimport sysimport multiWifiConnectgc.collect()led = machine.Pin('G13', machine.Pin.OUT)led.value(1)multiWifiConnect.connectAttempt()led.value(0)
