de8bce22c087435fb74cb2d04830a24a01b4424d
Nicolas Delgado <32339920+nadelgadoc@users.noreply.github.com>
2017-10-11 10:35:03 -0300

    Rename main32.py to main.py

--- main32.py
+++
@@ -1,1 +0,0 @@
-# This file is executed on every boot (including wake-boot from deepsleep)import gcimport machineimport osimport sysimport multiWifiConnectgc.collect()led = machine.Pin('G13', machine.Pin.OUT)led.value(1)multiWifiConnect.connectAttempt()led.value(0)
